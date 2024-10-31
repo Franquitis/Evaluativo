1736c7fb642bfea61d4eff7c3c50ce2080deba25
Franquitis <fq3730478@gmail.com>
2024-10-30 22:35:01 -0300

    Pagina terminada (CREO)

--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
-import { CardComponent } from './components/card/card.component';
+
 import { CarruselComponent } from './components/carrusel/carrusel.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 
@@ -16,7 +16,7 @@ import { CardInicioComponent } from '../producto/components/card-inicio/card-ini
 @NgModule({
   declarations: [
     InicioComponent,
-    CardComponent,
+
     CarruselComponent,
     CardInicioComponent
   ],
@@ -28,7 +28,7 @@ import { CardInicioComponent } from '../producto/components/card-inicio/card-ini
   ],
   exports:[
     InicioComponent,
-    CardComponent,
+
     CarruselComponent,
     MatGridListModule,
     MatIconModule,
